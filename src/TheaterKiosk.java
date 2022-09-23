ef0dd30ef1d67b31c67bef9216979d2e4300e21a
NathanDahlquist <nathan.j.dahlquist@gmail.com>
2022-09-23 17:57:34 -0400

    Task completed

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,12 @@
+public class TheaterKiosk {
+    public static void main(String[] args) {
+        var age = 0;
+        final var AGE_MINIMUM = 21;
+
+        System.out.println("Please enter your age.");
+        age = 17;
+        if(age >= 21){
+            System.out.println("You are eligible for a band!");
+        }
+    }
+}
